3ef03b2538e188e4737cb60aee6cd7318342ba73
haiba <9700samarth@gmail.com>
2025-05-21 21:33:08 +0530

    init

---
+++ Makefile
@@ -0,0 +1,2 @@
+xim: xim.c
+	$(CC) xim.c -o xim -Wall -Wextra -pedantic -std=c99
---
+++ xim.c
@@ -0,0 +1,6 @@
+#include <unistd.h>
+//https://viewsourcecode.org/snaptoken/kilo/02.enteringRawMode.html
+
+int main(){
+	return 0;
+}
